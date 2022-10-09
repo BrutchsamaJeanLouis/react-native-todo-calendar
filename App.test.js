f0785b3a3073852d11f9e84f713c696ff3f77ca4
BrutchsamaJeanLouis <73585384+BrutchsamaJeanLouis@users.noreply.github.com>
2022-10-09 17:05:15 +0100

    working app, no unit test,

--- App.test.js
+++ App.test.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
+import React from 'react'
+import renderer from 'react-test-renderer'
+import App from './App'
 
-import App from './App';
 
 describe('<App />', () => {
-  it('has 1 child', () => {
-    const tree = renderer.create(<App />).toJSON();
-    expect(tree.children.length).toBe(1);
-  });
-});
+  // it('has 1 child', () => {
+  //   const tree = renderer.create(<AppBar />).toJSON()
+  //   expect(tree.length).toBe(1) // The length of the tree should be three because we want a 3x3 grid
+  // })
+  it('renders correctly', () => {
+    const tree = renderer.create(<App />).toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+})
